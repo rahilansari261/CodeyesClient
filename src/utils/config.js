d097cf6456b3ce4704df88ce28d22a439cbf9e73
nexprism <136814920+nexprism@users.noreply.github.com>
2024-11-22 21:03:38 +0530

    Update config.js
    base url

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://127.0.0.1:8000/api/admin";
-export const IMAGE_BASE_URL = "http://127.0.0.1:8000/public/blog/images";
+export const BASE_URL = "https://everythinge.nexprism.in/api/admin";
+export const IMAGE_BASE_URL = "https://everythinge.nexprism.in/public/blog/images";
 // export const BASE_URL = 'https://devvrat-new-backend.onrender.com/api/admin'
 export const GOOGLE_MAPS_API_KEY = "AIzaSyBmcmf6w_4MAPQkuyvAZylbf28XvsO4kzs";  
